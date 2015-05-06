487b1a92872b8b25d2874bbd41e4d912f71bf5be
Alexander Kradenkov <alexk@trimm.local>
2015-05-06 13:23:04 +0300

    IDPDefineSamples.h -> added macro samples

---
+++ IDPStudy/IDPStudy/Source/IDPDefineSamples/IDPDefineSamples.h
@@ -0,0 +1,29 @@
+//
+//  IDPDefineSamples.h
+//  IDPStudy
+//
+//  Created by Alexander Kradenkov on 5/6/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#ifndef IDPStudy_IDPDefineSamples_h
+#define IDPStudy_IDPDefineSamples_h
+
+// header body
+int value = 1;
+
+#define IDPStringify(arg) \
+    #arg
+
+#define IDPNoArgOneLineMacro printf("no arg macro called\n")
+
+#define IDPNoArgMultiLineMacro \
+    printf("no arg macro called 1st line\n"); \
+    printf("no arg macro called 2nd line\n")
+
+#define IDPArgumentedMacro(arg1, arg2, arg3) \
+    printf("no arg macro called " IDPStringify(arg1) "\n"); \
+    printf("no arg macro called " IDPStringify(arg2) "\n"); \
+    printf("no arg macro called " IDPStringify(arg3) "\n")
+
+#endif
