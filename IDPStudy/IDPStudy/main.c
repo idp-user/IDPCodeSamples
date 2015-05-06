df90f7d7b0aad32738d3768af24bcc1b0f6557ee
Alexander Kradenkov <alexk@trimm.local>
2015-05-06 11:24:00 +0300

    IDPStudy Project -> initial commit

---
+++ IDPStudy/IDPStudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IDPStudy
+//
+//  Created by Alexander Kradenkov on 5/6/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
