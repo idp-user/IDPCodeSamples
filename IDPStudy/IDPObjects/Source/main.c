c05bc6b8b4ced2d6302bfe1bb7a0dd3a86beac2f
Alexander Kradenkov <alexk@trimm.local>
2015-05-12 14:30:16 +0300

    added IDPObjects target

---
+++ IDPStudy/IDPObjects/Source/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  IDPObjects
+//
+//  Created by Alexander Kradenkov on 5/12/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
