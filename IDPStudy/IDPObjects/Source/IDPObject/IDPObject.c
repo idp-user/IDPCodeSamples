93a29e0fc5473356ebb3f3e85be00aabc6a705ab
Alexander Kradenkov <alexk@trimm.local>
2015-05-12 14:32:05 +0300

    IDPObject -> initiali commit project.pbxproj -> added IDP class prefix

---
+++ IDPStudy/IDPObjects/Source/IDPObject/IDPObject.c
@@ -0,0 +1,9 @@
+//
+//  IDPObject.c
+//  IDPStudy
+//
+//  Created by Alexander Kradenkov on 5/12/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#include "IDPObject.h"
---
+++ IDPStudy/IDPObjects/Source/IDPObject/IDPObject.h
@@ -0,0 +1,14 @@
+//
+//  IDPObject.h
+//  IDPStudy
+//
+//  Created by Alexander Kradenkov on 5/12/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#ifndef __IDPStudy__IDPObject__
+#define __IDPStudy__IDPObject__
+
+#include <stdio.h>
+
+#endif /* defined(__IDPStudy__IDPObject__) */
