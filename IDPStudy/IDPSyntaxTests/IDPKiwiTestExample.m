614fd5caf9979328de973fa6b8b1ebd4d977ee84
Alexander Kradenkov <alexk@trimm.local>
2015-07-23 11:00:33 +0300

    Podfile -> pod 'Kiwi', '~> 2.3.1'

---
+++ IDPStudy/IDPSyntaxTests/IDPKiwiTestExample.m
@@ -0,0 +1,27 @@
+//
+//  IDPKiwiTestExample.m
+//  IDPStudy
+//
+//  Created by Alexander Kradenkov on 7/22/15.
+//  Copyright (c) 2015 IDAP College. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+
+
+SPEC_BEGIN(IDPKiwiExampleSpec)
+
+describe(@"Example description", ^{
+    context(@"NSObject", ^{
+        NSObject *object = [NSObject new];
+        it(@"should be of NSObject class", ^{
+            [[object should] beKindOfClass:[NSObject class]];
+        });
+        
+        it(@"shouldn't be nil", ^{
+            [[object should] beNil];
+        });
+    });
+});
+
+SPEC_END
